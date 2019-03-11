de3229ebb4888c444588dcb1908b800244fe670f
KarlaPina <48394610+karlapinam@users.noreply.github.com>
2019-03-10 21:57:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+
+//let textonew= ('hola')
+/*
+const cambiar = () => {
+ albumimg.src= textonew
+}
+*/
+
+/*
+function cambiar(){
+   albumimg.innerHTML = "Hello";
+  }
+  know.addEventListener('click',cambiar)
+*/
 let know= document.getElementById('boton')
 let albumimg= document.getElementById('albumimg')
 
+let casa= document.getElementById('casa')
 
-let textonew= ('hola')
-
-
-const cambiar = () => {
- albumimg.src= textonew
-  
+function knowmore(){
+document.getElementById('casa').innerHTML =
+"Hello Dolly!"
 }
-
-know.addEventListener('click', cambiar)
+know.addEventListener('click',knowmore)
